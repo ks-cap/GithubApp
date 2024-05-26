e31b7fa58bb1b57f8317b64b7fec68c7b45fecf3
Ken Sato <sato.ken@moneyforward.co.jp>
2024-05-26 19:23:51 +0900

    Initial Commit

---
+++ GithubApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubApp
+//
+//  Created by sato.ken on 2024/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubApp/GithubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAppApp.swift
+//  GithubApp
+//
+//  Created by sato.ken on 2024/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GithubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
